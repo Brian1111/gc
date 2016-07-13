16dd00b9c9a866def76987b173eef5b8e315891f
H S <H S>
2016-07-12 23:10:39 -0700

    first commit

---
+++ routes/index3.js
@@ -0,0 +1,17 @@
+var config = require('../config');
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')(config.dbURI);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var db = req.db;
+    var posts = db.get('posts');
+    posts.find({}, {}, function(err, posts) {
+        res.render('index3', { posts: posts });
+    });
+});
+
+
+module.exports = router;
